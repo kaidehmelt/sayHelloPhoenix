cdb27745643fbf3102a0c55f5dbe52e3f41a9be4
Kai Dehmelt <kaidehmelt@posteo.de>
2023-12-11 21:24:26 +0100

    initial commit

---
+++ sayHelloPhoenix.cpp
@@ -0,0 +1,12 @@
+﻿// sayHelloPhoenix.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "sayHelloPhoenix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Phoenix Contact." << endl;
+	return 0;
+}
